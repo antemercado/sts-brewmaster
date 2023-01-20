6696e7410278a42ef140418cbb103ed31fca10fd
Anthony Mercado <mercado.anthony.e@gmail.com>
2023-01-20 16:31:15 -0500

    Update Default Cards to Basic

--- theBrewmaster/src/main/java/theBrewmaster/cards/DefendBrewmaster.java
+++ theBrewmaster/src/main/java/theBrewmaster/cards/DefendBrewmaster.java
@@ -23,7 +23,7 @@ public class DefendBrewmaster extends AbstractDynamicCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.BASIC;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheBrewmaster.Enums.COLOR_GRAY;
--- theBrewmaster/src/main/java/theBrewmaster/cards/StrikeBrewmaster.java
+++ theBrewmaster/src/main/java/theBrewmaster/cards/StrikeBrewmaster.java
@@ -25,7 +25,7 @@ public class StrikeBrewmaster extends AbstractDynamicCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.BASIC;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheBrewmaster.Enums.COLOR_GRAY;
