95712fc915774ae9ea994eb66d9fac353899e4ab
Anthony Mercado <mercado.anthony.e@gmail.com>
2023-01-26 23:30:45 -0500

    Add Burning Breath

---
+++ theBrewmaster/src/main/java/theBrewmaster/cards/BurningBreath.java
@@ -0,0 +1,63 @@
+package theBrewmaster.cards;
+
+import basemod.AutoAdd;
+import basemod.abstracts.CustomCard;
+import basemod.helpers.BaseModCardTags;
+import theBrewmaster.BrewmasterMod;
+import theBrewmaster.characters.BrewmasterCharacter;
+
+import static theBrewmaster.BrewmasterMod.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+public class BurningBreath extends AbstractDynamicCard {
+    // TEXT DECLARATION
+    public static final String ID = BrewmasterMod.makeID(BurningBreath.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = BrewmasterCharacter.Enums.ORANGE;
+
+    private static final int COST = 1;
+
+    private static final int DAMAGE = 7;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+    private static final int MAGIC = 1;
+    private static final int UPGRADE_PLUS_MAGIC = 1;
+
+    public BurningBreath() { 
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = this.damage = DAMAGE;
+        this.baseMagicNumber = this.magicNumber = MAGIC;
+    }
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
+    }
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC);
+            initializeDescription();
+        }
+    }
+}
