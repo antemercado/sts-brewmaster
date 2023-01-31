3050f528af9dcc559de2c92426e29c4b7b5099a7
Anthony Mercado <mercado.anthony.e@gmail.com>
2023-01-30 20:52:31 -0500

    Added Twisted Brew

---
+++ theBrewmaster/src/main/java/theBrewmaster/cards/TwistedBrew.java
@@ -0,0 +1,57 @@
+package theBrewmaster.cards;
+
+import basemod.AutoAdd;
+import basemod.abstracts.CustomCard;
+import basemod.helpers.BaseModCardTags;
+import theBrewmaster.BrewmasterMod;
+import theBrewmaster.characters.BrewmasterCharacter;
+import theBrewmaster.enums.CustomTags;
+
+import static theBrewmaster.BrewmasterMod.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.TransformCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class TwistedBrew extends AbstractDynamicCard {
+    // TEXT DECLARATION
+    public static final String ID = BrewmasterMod.makeID(TwistedBrew.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = BrewmasterCharacter.Enums.ORANGE;
+
+    private static final int COST = 1;
+    private static final int UPGRADED_COST = 0;
+
+    public TwistedBrew() { 
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+
+        this.tags.add(CustomTags.BREW);
+    }
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int i = AbstractDungeon.cardRng.random(0, p.hand.size() - 2); // -1 for zero-index, -1 because game counts the card thats being played as part of hand size. Need to reduce that by 1.
+        addToBot(new TransformCardInHandAction(i, AbstractDungeon.returnTrulyRandomCard()));
+    }
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
