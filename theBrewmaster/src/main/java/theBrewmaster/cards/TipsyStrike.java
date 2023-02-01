d9d58b5a8e7854488a09bbad027a2bba48c9f84e
Anthony Mercado <mercado.anthony.e@gmail.com>
2023-02-01 18:47:29 -0500

    Added TipsyStrike

---
+++ theBrewmaster/src/main/java/theBrewmaster/cards/TipsyStrike.java
@@ -0,0 +1,65 @@
+package theBrewmaster.cards;
+
+import basemod.AutoAdd;
+import basemod.abstracts.CustomCard;
+import basemod.helpers.BaseModCardTags;
+import theBrewmaster.BrewmasterMod;
+import theBrewmaster.characters.BrewmasterCharacter;
+
+import static theBrewmaster.BrewmasterMod.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class TipsyStrike extends AbstractDynamicCard {
+    // TEXT DECLARATION
+    public static final String ID = BrewmasterMod.makeID(TipsyStrike.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = BrewmasterCharacter.Enums.ORANGE;
+
+    private static final int COST = 1;
+
+    private static final int DAMAGE = 11;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+    public TipsyStrike() { 
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = this.damage = DAMAGE;
+
+        this.cardsToPreview = new VoidCard();
+
+        this.tags.add(CardTags.STRIKE);
+    }
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractCard c = new VoidCard();
+
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        addToBot(new MakeTempCardInDrawPileAction(c, 1, true, true));
+    }
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
