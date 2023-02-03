24fb31b72fed5200d5a72e05f4014902d7a98b0e
Anthony Mercado <mercado.anthony.e@gmail.com>
2023-02-02 22:36:56 -0500

    Add ImportedBrew

---
+++ theBrewmaster/src/main/java/theBrewmaster/actions/ImportedBrewAction.java
@@ -0,0 +1,119 @@
+package theBrewmaster.actions;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.helpers.CardLibrary.LibraryType;
+import com.megacrit.cardcrawl.screens.CardRewardScreen;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToDiscardEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToHandEffect;
+
+import basemod.BaseMod;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardRarity;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardTags;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+
+public class ImportedBrewAction extends AbstractGameAction{
+
+    private boolean retrieveCard = false;
+    private boolean upgraded;
+
+    public ImportedBrewAction(boolean upgraded) {
+        this.actionType = ActionType.CARD_MANIPULATION;
+        this.duration = Settings.ACTION_DUR_FAST;
+        this.upgraded = upgraded;
+    }
+
+    @Override
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_FAST){
+            AbstractDungeon.cardRewardScreen.customCombatOpen(generateCardChoices(), CardRewardScreen.TEXT[1], true);
+            tickDuration();
+            return;
+        }
+        if (!this.retrieveCard){
+            if (AbstractDungeon.cardRewardScreen.discoveryCard != null){
+                AbstractCard disCard = AbstractDungeon.cardRewardScreen.discoveryCard.makeStatEquivalentCopy();
+                disCard.current_x = -1000.0f * Settings.xScale;
+                if (AbstractDungeon.player.hand.size() < BaseMod.MAX_HAND_SIZE){
+                    AbstractDungeon.effectList.add(new ShowCardAndAddToHandEffect(disCard, Settings.WIDTH / 2.0f, Settings.HEIGHT / 2.0f));
+                } else {
+                    AbstractDungeon.effectList.add(new ShowCardAndAddToDiscardEffect(disCard, Settings.WIDTH / 2.0f, Settings.HEIGHT / 2.0f));
+                }
+                AbstractDungeon.cardRewardScreen.discoveryCard = null;
+            }
+            this.retrieveCard = true;
+        }
+        tickDuration();
+    }
+
+    private ArrayList<AbstractCard> generateCardChoices() {
+        ArrayList<AbstractCard> ret = new ArrayList<>();
+
+        // Generate Hash Map with all character's card pools
+        ArrayList<AbstractPlayer> chars = CardCrawlGame.characterManager.getAllCharacters();
+        HashMap<CardColor,ArrayList<AbstractCard>> cards = new HashMap<>(); 
+        for (AbstractPlayer p: chars){
+            CardColor color = p.getCardColor();
+            if (p.getCardColor() != AbstractDungeon.player.getCardColor()){
+                ArrayList<AbstractCard> tmp = new ArrayList<>();
+                cards.put(color, p.getCardPool(tmp));
+            }
+        }
+
+        // Generte Card Rewards
+        while (ret.size() != 3){
+            AbstractCard.CardRarity cardRarity;
+            
+            int roll = AbstractDungeon.cardRandomRng.random(99);
+            if (roll < 55){
+                cardRarity = CardRarity.COMMON;
+            } else if (roll < 85) {
+                cardRarity = CardRarity.UNCOMMON;
+            } else {
+                cardRarity = CardRarity.RARE;
+            }
+
+            // Determine which character to use.
+            int charIndex = AbstractDungeon.cardRandomRng.random(cards.size() - 1);
+            Object charKey = cards.keySet().toArray()[charIndex];
+            ArrayList<AbstractCard> charCards = cards.get(charKey);
+            
+            // Populate CardGroup with cards.
+            CardGroup anyCard = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+            for (AbstractCard c: charCards){
+                if (c.rarity == cardRarity && !c.hasTag(CardTags.HEALING) && c.type != CardType.CURSE && c.type != CardType.STATUS && !UnlockTracker.isCardLocked(c.cardID)){
+                    anyCard.addToBottom(c);
+                }
+            }
+            
+            // Return card.
+            anyCard.shuffle(AbstractDungeon.cardRandomRng);
+            AbstractCard retCard = anyCard.getRandomCard(true);
+            if (this.upgraded){
+                retCard.upgrade();
+            }
+            ret.add(retCard);
+            
+            // No longer need the character in the HashMap
+            cards.remove(charKey);
+        }
+
+        return ret;
+    }
+    
+}
---
+++ theBrewmaster/src/main/java/theBrewmaster/cards/ImportedBrew.java
@@ -0,0 +1,57 @@
+package theBrewmaster.cards;
+
+import basemod.AutoAdd;
+import basemod.abstracts.CustomCard;
+import basemod.helpers.BaseModCardTags;
+import theBrewmaster.BrewmasterMod;
+import theBrewmaster.actions.ImportedBrewAction;
+import theBrewmaster.characters.BrewmasterCharacter;
+import theBrewmaster.enums.CustomTags;
+
+import static theBrewmaster.BrewmasterMod.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class ImportedBrew extends AbstractDynamicCard {
+    // TEXT DECLARATION
+    public static final String ID = BrewmasterMod.makeID(ImportedBrew.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = BrewmasterCharacter.Enums.ORANGE;
+
+    private static final int COST = 0;
+
+    public ImportedBrew() { 
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.exhaust = true;
+
+        this.tags.add(CustomTags.BREW);
+    }
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ImportedBrewAction(upgraded));
+    }
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            initializeDescription();
+        }
+    }
+}
