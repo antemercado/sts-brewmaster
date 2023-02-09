1dcc6b343da6f4418f7eca7ed6c0dc185f27dae4
Anthony Mercado <mercado.anthony.e@gmail.com>
2023-02-08 21:21:22 -0500

    Add Detanator Strike

---
+++ theBrewmaster/src/main/java/theBrewmaster/cards/DetonatorStrike.java
@@ -0,0 +1,75 @@
+package theBrewmaster.cards;
+
+import basemod.AutoAdd;
+import basemod.abstracts.CustomCard;
+import basemod.helpers.BaseModCardTags;
+import theBrewmaster.BrewmasterMod;
+import theBrewmaster.characters.BrewmasterCharacter;
+import theBrewmaster.enums.CustomTags;
+import theBrewmaster.powers.DrenchedPower;
+
+import static theBrewmaster.BrewmasterMod.makeCardPath;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class DetonatorStrike extends AbstractDynamicCard {
+    // TEXT DECLARATION
+    public static final String ID = BrewmasterMod.makeID(DetonatorStrike.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = BrewmasterCharacter.Enums.ORANGE;
+
+    private static final int COST = 1;
+
+    private static final int MAGIC = 4;
+    private static final int UPGRADE_PLUS_MAGIC = 2;
+
+    public DetonatorStrike() { 
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = this.magicNumber = MAGIC;
+
+        this.tags.add(CardTags.STRIKE);
+    }
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.damage = 0;
+        if (m.hasPower(DrenchedPower.POWER_ID)){
+            this.damage = m.getPower(DrenchedPower.POWER_ID).amount * magicNumber;
+        }
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+    }
+
+    // Glow when Intoxicated
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters){
+            if (mo.hasPower(DrenchedPower.POWER_ID)){
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                return;
+            }
+        }
+    }
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC);
+            initializeDescription();
+        }
+    }
+}
