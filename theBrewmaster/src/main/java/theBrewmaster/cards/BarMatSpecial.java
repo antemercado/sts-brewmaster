7f6effab2f4620f2050875946481c087b90c8725
Anthony Mercado <mercado.anthony.e@gmail.com>
2024-09-30 03:32:12 -0400

    BarMatSpecial: Multiattack

--- theBrewmaster/src/main/java/theBrewmaster/cards/BarMatSpecial.java
+++ theBrewmaster/src/main/java/theBrewmaster/cards/BarMatSpecial.java
@@ -11,6 +11,7 @@ import static theBrewmaster.BrewmasterMod.makeCardPath;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -29,10 +30,11 @@ public class BarMatSpecial extends AbstractBrewmasterCard {
     
     private static final int COST = 1;
     
-    private static final int DAMAGE = 0;
-    
-    private static final int MAGIC = 2;
-    private static final int UPGRADE_PLUS_MAGIC = 1;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DAMAGE = 3;
+
+    private static final int MAGIC = 0;
+
     
     // TEXT DECLARATION
     public static final String ID = BrewmasterMod.makeID(BarMatSpecial.class.getSimpleName());
@@ -45,7 +47,7 @@ public class BarMatSpecial extends AbstractBrewmasterCard {
     public BarMatSpecial() { 
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         this.baseDamage = this.damage = DAMAGE;
-        this.baseMagicNumber = this.magicNumber = MAGIC;
+        this.baseMagicNumber = 0;
     }
     // Actions the card should do.
     @Override
@@ -53,26 +55,44 @@ public class BarMatSpecial extends AbstractBrewmasterCard {
 
         AttackEffect effect = AbstractGameAction.AttackEffect.BLUNT_LIGHT;
 
-        if (BrewmasterMod.brewCardsPlayedThisCombat > 10){
-            effect = AbstractGameAction.AttackEffect.BLUNT_HEAVY;
+        for (int i = 0; i < BrewmasterMod.brewCardsPlayedThisTurn; i++){
+            addToBot(new DamageAction(m, new DamageInfo(p, this.baseDamage, damageTypeForTurn), effect));
         }
-        addToBot(new DamageAction(m, new DamageInfo(p, this.baseDamage, damageTypeForTurn), effect));
+
     }
 
+    // public void applyPowers() {
+    //     if (BrewmasterMod.brewCardsPlayedThisTurn > 0){
+    //         this.finalDamage = BrewmasterMod.brewCardsPlayedThisTurn * this.baseDamage;
+    //         super.applyPowers();
+    //         this.rawDescription = DESCRIPTION + EXTENDED_DESCRIPTION[0];
+    //         initializeDescription();
+    //     }
+    // }
+
     public void applyPowers() {
-        if (BrewmasterMod.brewCardsPlayedThisCombat > 0){
-            this.baseDamage = BrewmasterMod.brewCardsPlayedThisCombat * this.magicNumber;
+        if (BrewmasterMod.brewCardsPlayedThisTurn > 0){
+            this.magicNumber = BrewmasterMod.brewCardsPlayedThisTurn;
             super.applyPowers();
             this.rawDescription = DESCRIPTION + EXTENDED_DESCRIPTION[0];
             initializeDescription();
         }
     }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        if (BrewmasterMod.brewCardsPlayedThisTurn > 0){
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        }
+    }
+
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_MAGIC);
+            upgradeDamage(UPGRADE_PLUS_DAMAGE);
             initializeDescription();
         }
     }
